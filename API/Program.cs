1c4006501c796927ddc939b2012af2ab07f23bd7
Jesse Castro <jcastro@jaisoftawre.net>
2026-02-12 03:42:15 +0800

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,55 @@
+
+using API.DAL;
+using API.Services;
+using System.Text.Json;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    // Replace with the actual URL/Port your UI project is running on
+                    policy.WithOrigins("http://localhost:5043", "https://localhost:5043")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            builder.Services.AddSingleton<ProductRepository>();
+            builder.Services.AddScoped<ProductService>();
+
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseRouting();
+            app.UseCors("AllowFrontend");
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
